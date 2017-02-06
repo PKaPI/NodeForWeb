9c8a239fa2637af4aef7a1aaa3c6e710682f64eb
皮振 <itpizhen@163.com>
2017-02-06 16:47:03 +0800

    登录页静态资源解析

--- myapp/app.js
+++ myapp/app.js
@@ -4,26 +4,35 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session=require('express-session');//使用session
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/home/index');
+var users = require('./routes/admin/login');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html',require('ejs').__express);//设置视图目录及目标引擎
+app.set('view engine', 'html');
 
+app.use(session({
+    secret:'user',
+    resave:false,
+    saveUninitalized:true,
+    cookie:{maxAge:1000*3600}
+}));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev')); //日志
+app.use(bodyParser.json()); //处理表单请求并转成json格式
+app.use(bodyParser.urlencoded({ extended: false }));//处理表单
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //内置中间件，解析静态资源
+app.use(express.static(path.join(__dirname, 'views')));//解析静态资源
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+app.use('/admin/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
